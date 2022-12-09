35fefd05628eda251b8db5890640e760b1b7e48e
Alex Zhuang <azhuang639@gmail.com>
2022-12-09 19:26:20 +0100

    camera follows smoothly

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -11,7 +11,7 @@ public class FollowPlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = player.transform.position + offset;
     }
 
     // Update is called once per frame
