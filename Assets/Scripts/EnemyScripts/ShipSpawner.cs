23fd3c119e95c3f38b4bff799474c732b0467ffb
Alex Zhuang <azhuang639@gmail.com>
2022-12-13 15:44:24 +0100

    added ship for player to main, destroys ships now

--- Assets/Scripts/EnemyScripts/ShipSpawner.cs
+++ Assets/Scripts/EnemyScripts/ShipSpawner.cs
@@ -9,6 +9,8 @@ public class ShipSpawner : MonoBehaviour
     [SerializeField] private float minSeparationTime;
     [SerializeField] private float maxSeparationTime;
 
+    private List<GameObject> currentShips = new List<GameObject>();
+
     private void Start()
     {
         StartCoroutine(SpawnShip()); 
@@ -21,9 +23,17 @@ public class ShipSpawner : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(Random.Range(minSeparationTime, maxSeparationTime));
-            Instantiate(ship, spawnPos.position, Quaternion.identity);
+            currentShips.Add(Instantiate(ship, spawnPos.position, Quaternion.identity));
         }
      
+    }
+
+    private void OnDestroy()
+    {
+        foreach (GameObject currentShip in currentShips)
+        {
+            Destroy(currentShip);
+        }
     }
 }
 
